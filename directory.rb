2d6e829307f3063bad9fc534935a793a2c891912
Jordan <jordanmanu@live.com.au>
2022-03-04 10:59:04 +0000

    Refactored with methods

--- directory.rb
+++ directory.rb
@@ -1,17 +1,32 @@
-student_count = 11
+students = [
+  "Dr. Hannibal Lecter",
+  "Darth Vader",
+  "Nurse Ratched",
+  "Michael Corleone",
+  "Alex DeLarge",
+  "The Wicked Witch of the West",
+  "Terminator",
+  "Freddy Krueger",
+  "The Joker",
+  "Joffrey Baratheon",
+  "Norman Bates"
+]
 
-puts "The students of Villains Academy"
-puts "------------"
-puts "Dr. Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex DeLarge"
-puts "The Wicked Witch of the West"
-puts "Terminator"
-puts "Freddy Krueger"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Norman Bates"
-# Print total number of students
-puts "Overall, we have #{student_count} great students."
+def print_header
+  puts "The students of Villains Academy"
+  puts "-----------"
+end
+
+def print(names)
+  names.each do |name|  
+   puts name
+  end
+end
+
+def print_footer(names)
+ puts "Overall, we have #{names.count} great students."
+end
+
+print_header
+print(students)
+print_footer(students)

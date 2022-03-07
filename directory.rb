d0916042fe13b18b86a6ab2abc023541c2c13e14
Jordan <jordanmanu@live.com.au>
2022-03-07 15:59:04 +0000

    Added save students - saving data to students.csv

--- directory.rb
+++ directory.rb
@@ -1,15 +1,59 @@
 @students = []
 
+def save_students
+  file = File.open("students.csv", "w")
+  @students.each do |student|
+  student_data = [student[:name], student[:cohort]]
+  csv_line = student_data.join(",")
+  file.puts csv_line
+  end
+  file.close
+end
+
 def input_students
-#   puts "Please enter the names of the students"
-#   puts "To finish, just hit return twice"
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
   name = gets.chomp
   while !name.empty? do
     @students << {name: name, cohort: :november}
     puts "Now we have #{@students.count} students"
     name = gets.chomp
   end
-  @students
+end
+
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "3. Save the list to students.csv"
+  puts "9. Exit"
+end
+
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
+
+def process(selection)
+  case selection
+    when "1"
+      input_students
+    when "2"
+      show_students
+    when "3"
+      save_students
+    when "9"
+      exit
+    else
+      puts "I don't know what you meant, try again"
+  end
 end
 
 def print_header
@@ -26,37 +70,5 @@ end
 def print_footer
  puts "Overall, we have #{@students.count} great students."
 end
-
-def print_menu
-  puts "1. Input the students"
-  puts "2. Show the students"
-  puts "9. Exit"
-end
-
-def show_students
-  print_header
-  print_students_list
-  print_footer
-end
-
-def interactive_menu
-  loop do
-    print_menu
-    process(gets.chomp)
-  end
-end
-
-  def process(selection)
-    case selection
-      when "1"
-        input_students
-      when "2"
-        show_students
-      when "9"
-        exit
-      else
-        puts "I don't know what you meant, try again"
-    end
-  end
 
 interactive_menu
